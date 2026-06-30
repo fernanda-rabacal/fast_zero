a5ce1c896c986955cdf0a247a78a39f8914d6a05
Fernanda Rabaçal <nandarabacal02@hotmail.com>
2026-06-30 15:37:24 -0300

    Refatorando estrutura do projeto: Suporte a asyncio, tornando o projeto não bloqueante

--- migrations/env.py
+++ migrations/env.py
@@ -1,8 +1,10 @@
+import asyncio
 
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
@@ -21,6 +23,29 @@ target_metadata = table_registry.metadata
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+
+def do_run_migrations(connection): 
+    context.configure(connection=connection, target_metadata=target_metadata)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_async_migrations(): 
+    connectable = async_engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await connectable.dispose()
+
+
+def run_migrations_online(): 
+    asyncio.run(run_async_migrations())
 
 
 def run_migrations_offline() -> None:
@@ -45,29 +70,6 @@ def run_migrations_offline() -> None:
 
     with context.begin_transaction():
         context.run_migrations()
-
-
-def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
-
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
-
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
-
-        with context.begin_transaction():
-            context.run_migrations()
-
 
 if context.is_offline_mode():
     run_migrations_offline()
--- routers/users.py
+++ routers/users.py
@@ -4,7 +4,7 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_session
 from models import User
@@ -16,23 +16,25 @@ from security import (
 
 router = APIRouter(prefix='/users', tags=['users'])
 
-AppSession = Annotated[Session, Depends(get_session)]
+AppSession = Annotated[AsyncSession, Depends(get_session)]
 CurrentUser = Annotated[User, Depends(get_current_user)]
 
 
 @router.get('/', response_model=UserList)
-def read_users(
+async def read_users(
     session: AppSession, filter_users: Annotated[FilterPage, Query()]
 ):
-    users = session.scalars(
+    query = await session.scalars(
         select(User).offset(filter_users.offset).limit(filter_users.limit)
-    ).all()
+    )
+    users = query.all()
+
     return {'users': users}
 
 
 @router.get('/{user_id}', response_model=UserPublic)
-def get_user(user_id: int, session: AppSession):
-    user = session.scalar(select(User).where((User.id == user_id)))
+async def get_user(user_id: int, session: AppSession):
+    user = await session.scalar(select(User).where((User.id == user_id)))
 
     if user is None:
         raise HTTPException(
@@ -43,8 +45,8 @@ def get_user(user_id: int, session: AppSession):
 
 
 @router.post('/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema, session: AppSession):
-    db_user = session.scalar(
+async def create_user(user: UserSchema, session: AppSession):
+    db_user = await session.scalar(
         select(User).where(
             (User.username == user.username) | (User.email == user.email)
         )
@@ -70,14 +72,14 @@ def create_user(user: UserSchema, session: AppSession):
         password=hashed_password,
     )
     session.add(db_user)
-    session.commit()
-    session.refresh(db_user)
+    await session.commit()
+    await session.refresh(db_user)
 
     return db_user
 
 
 @router.put('/{user_id}', response_model=UserPublic)
-def update_user(
+async def update_user(
     user_id: int,
     user: UserSchema,
     session: AppSession,
@@ -92,8 +94,8 @@ def update_user(
         current_user.username = user.username
         current_user.password = get_password_hash(user.password)
         current_user.email = user.email
-        session.commit()
-        session.refresh(current_user)
+        await session.commit()
+        await session.refresh(current_user)
 
         return current_user
 
@@ -105,7 +107,7 @@ def update_user(
 
 
 @router.delete('/{user_id}', response_model=Message)
-def delete_user(
+async def delete_user(
     user_id: int,
     session: AppSession,
     current_user: CurrentUser,
@@ -115,7 +117,7 @@ def delete_user(
             status_code=HTTPStatus.FORBIDDEN, detail='Not enough permissions'
         )
 
-    session.delete(current_user)
-    session.commit()
+    await session.delete(current_user)
+    await session.commit()
 
     return {'message': 'User deleted'}
